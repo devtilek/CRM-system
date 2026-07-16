227c3ee7eef12ba06720ddc60c3517c7ea1e4e14
Aktilek <kalabaevbakitjan@gmail.com>
2026-07-16 18:39:41 +0500

    Fix exception handler 16.07.26

--- src/main/java/practice/springcrm/exception/GlobalExceptionHandler.java
+++ src/main/java/practice/springcrm/exception/GlobalExceptionHandler.java
@@ -1,25 +1,50 @@
 package practice.springcrm.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
 import practice.springcrm.dto.ErrorResponse;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex){
+        Map<String, String> feildErrors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            feildErrors.put(error.getField(), error.getDefaultMessage());
+        }
+
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Ошибка валидации");
+        body.put("errors", feildErrors);
+        body.put("timestamp", LocalDateTime.now());
+
+        return  new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(EntityNotFoundException ex){
+        practice.springcrm.dto.ErrorResponse errorResponse = new practice.springcrm.dto.ErrorResponse(ex.getMessage(), null, LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handlGlobalException(Exception ex){
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex){
         ErrorResponse error = new ErrorResponse(
                 "Ошибка на сервере",
                 ex.getMessage(),
                 LocalDateTime.now()
         );
-        return new ResponseEntity<>(error,
-                HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
