6608d0a65b07607abadcc4cfeb0e33351b45b9bd
Aktilek <kalabaevbakitjan@gmail.com>
2026-07-16 18:50:16 +0500

    updated UserImpl: добавление роли пользователю

--- src/main/java/practice/springcrm/service/impl/UserImpl.java
+++ src/main/java/practice/springcrm/service/impl/UserImpl.java
@@ -3,13 +3,17 @@ package practice.springcrm.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import practice.springcrm.dto.JwtResponse;
 import practice.springcrm.dto.SignInRequest;
 import practice.springcrm.dto.SignUpRequest;
 import practice.springcrm.dto.UserDTO;
+import practice.springcrm.entity.Role;
 import practice.springcrm.entity.User;
 import practice.springcrm.mapper.UserMapper;
 import practice.springcrm.repository.UserRepo;
+import practice.springcrm.security.JwtProvider;
 import practice.springcrm.service.UserService;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class UserImpl implements UserService {
 
     private final UserRepo userRepo;
     private final UserMapper userMapper;
+    private final JwtProvider jwtProvider;
 
     @Override
     public UserDTO registerUser(SignUpRequest signUpRequest) {
@@ -34,6 +39,13 @@ public class UserImpl implements UserService {
 
         User user = userMapper.toEntity(signUpRequest);
 
+        try {
+            Role role = Role.valueOf(signUpRequest.getRole().toUpperCase());
+            user.setRole(role);
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("Неверно указано роль пользователя");
+        }
+
         User savedUser = userRepo.save(user);
 
         return userMapper.toDTO(savedUser);
@@ -41,14 +53,15 @@ public class UserImpl implements UserService {
 
     @Override
     @Transactional(readOnly = true)
-    public UserDTO loginUser(SignInRequest signInRequest) {
+    public JwtResponse loginUser(SignInRequest signInRequest) {
         User user = userRepo.findByEmail(signInRequest.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("Неверный Email или password"));
 
         if (!user.getPassword().equals(signInRequest.getPassword())){
             throw new IllegalArgumentException("Неверный Email или password");
         }
+        String token = jwtProvider.generateToken(user);
 
-        return userMapper.toDTO(user);
+        return new JwtResponse(token, user.getUsername());
     }
 }
