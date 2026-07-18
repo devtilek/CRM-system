7e686d2af0451eaa91aa0ddd079b4406f3bbc852
Aktilek <kalabaevbakitjan@gmail.com>
2026-07-18 14:49:59 +0500

    implement new method

--- src/main/java/practice/springcrm/service/CourseService.java
+++ src/main/java/practice/springcrm/service/CourseService.java
@@ -1,6 +1,7 @@
 package practice.springcrm.service;
 
 import practice.springcrm.dto.CourseDTO;
+import practice.springcrm.dto.UserDTO;
 
 import java.util.List;
 
@@ -9,4 +10,7 @@ public interface CourseService {
     List<CourseDTO> getAllCourses();
     void addCourse(CourseDTO courseDTO, Long userId);
     void deleteCourse(Long id);
+    void enrollStudent(Long courseId, Long studentId);
+    List<CourseDTO> getCoursesByTeacher(Long teacherId);
+    List<UserDTO> getStudentsByCourse(Long courseId);
 }
--- src/main/java/practice/springcrm/service/impl/CourseImpl.java
+++ src/main/java/practice/springcrm/service/impl/CourseImpl.java
@@ -1,24 +1,26 @@
 package practice.springcrm.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import practice.springcrm.dto.CourseDTO;
+import practice.springcrm.dto.UserDTO;
 import practice.springcrm.entity.Course;
+import practice.springcrm.entity.Role;
 import practice.springcrm.entity.User;
 import practice.springcrm.mapper.CourseMapper;
+import practice.springcrm.mapper.UserMapper;
 import practice.springcrm.repository.CourseRepo;
 import practice.springcrm.repository.UserRepo;
 import practice.springcrm.service.CourseService;
-
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CourseImpl implements CourseService {
     private final CourseMapper courseMapper;
+    private final UserMapper userMapper;
     private final CourseRepo courseRepo;
     private final UserRepo userRepo;
 
@@ -37,10 +39,13 @@ public class CourseImpl implements CourseService {
 
     @Override
     @Transactional
-    public void addCourse(CourseDTO courseDTO, Long userId) {
-        User user = userRepo.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
+    public void addCourse(CourseDTO courseDTO, Long teacherId) {
+        User teacher = userRepo.findById(teacherId).orElseThrow(() ->
+                new EntityNotFoundException("Teacher not found with id: " + teacherId));
+        if (teacher.getRole() != Role.ROLE_TEACHER){
+            throw new IllegalArgumentException("The user with this ID is not a teacher");
+        }
         Course course = courseMapper.toEntity(courseDTO);
-        course.setUser(user);
         courseRepo.save(course);
     }
 
@@ -50,5 +55,31 @@ public class CourseImpl implements CourseService {
             throw new EntityNotFoundException("Course not found with id: " + id);
         }
         courseRepo.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void enrollStudent(Long courseId, Long studentId) {
+        Course course = courseRepo.findById(courseId).orElseThrow(() -> new EntityNotFoundException("Course not found"));
+        User student = userRepo.findById(studentId).orElseThrow(() -> new EntityNotFoundException("Student not found"));
+        course.getStudents().add(student);
+        courseRepo.save(course);
+    }
+
+    @Override
+    public List<CourseDTO> getCoursesByTeacher(Long teacherId) {
+        return courseRepo.findByTeacherId(teacherId).stream()
+                .map(courseMapper ::toDTO)
+                .toList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserDTO> getStudentsByCourse(Long courseId) {
+        Course course = courseRepo.findById(courseId).orElseThrow(() ->
+                new EntityNotFoundException("Course not found"));
+        return course.getStudents().stream()
+                .map(userMapper::toDTO)
+                .toList();
     }
 }
