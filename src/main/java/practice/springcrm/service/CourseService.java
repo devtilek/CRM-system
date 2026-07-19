cece30f22f83d9b6a6830ec4876d247276085953
Aktilek <kalabaevbakitjan@gmail.com>
2026-07-19 21:49:33 +0500

    updated

--- src/main/java/practice/springcrm/service/CourseService.java
+++ src/main/java/practice/springcrm/service/CourseService.java
@@ -1,5 +1,6 @@
 package practice.springcrm.service;
 
+import practice.springcrm.dto.CourseCreateDTO;
 import practice.springcrm.dto.CourseDTO;
 import practice.springcrm.dto.UserDTO;
 
@@ -8,9 +9,10 @@ import java.util.List;
 public interface CourseService {
     CourseDTO getCourse(Long id);
     List<CourseDTO> getAllCourses();
-    void addCourse(CourseDTO courseDTO, Long userId);
+    void addCourse(CourseCreateDTO courseCreateDTO, Long userId);
     void deleteCourse(Long id);
     void enrollStudent(Long courseId, Long studentId);
     List<CourseDTO> getCoursesByTeacher(Long teacherId);
     List<UserDTO> getStudentsByCourse(Long courseId);
+
 }
