e349cf621c04caa7767a5f3cf81bf42eea5c820c
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-05 12:09:36 +0500

    test: isolate application context test

--- src/test/java/practice/springcrm/SpringCrmApplicationTests.java
+++ src/test/java/practice/springcrm/SpringCrmApplicationTests.java
@@ -2,12 +2,13 @@ package practice.springcrm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringCrmApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
